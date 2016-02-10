29b6fbca03003d7785fdac335c7fd1395c57bd35
am-consulting <contact@am-consulting.co.jp>
2016-02-10 18:00:29 +0900

    boxplotにデートフォームを追加
    boxplotにデートフォームを追加

--- R/boxplotbyleader.r
+++ R/boxplotbyleader.r
@@ -5,7 +5,7 @@
 #'@return titleAbb+Boxplot
 #'
 #'@export
-boxplotbyleader<-function(dataSource=""){
+boxplotbyleader<-function(dataSource="" , dateform="%Y/%m"){
   for(jjj in 1:length(indextitle)){#1:PMJ,2:BOJ
     tmp<-dataset[,c(2,2+jjj)]
     colnames(tmp)[2]<-"Person"
@@ -13,10 +13,10 @@ boxplotbyleader<-function(dataSource=""){
     boxplotData<-boxplotData[,-2]
     colnames(boxplotData)[1]<-"variable"
     boxplotData[,2]<-as.numeric(boxplotData[,2])
-    assign(paste(titleAbb,"Boxplot",sep="")[jjj],boxplotData,env=.GlobalEnv)
+    assign(paste(titleAbb,"Boxplot",sep="")[jjj],boxplotData,envir = .GlobalEnv)
     g<-ggplot(data=boxplotData,aes(x=variable,y=value))
     g<-g+geom_boxplot(lwd=1)
-    g<-g+ggtitle(paste(colnames(dataset)[2],"\n",first(dataset[,1])," - ",last(dataset[,1]),",Source:",dataSource,sep=""))
+    g<-g+ggtitle(paste(colnames(dataset)[2],"\n",format(first(dataset[,1]),dateform)," - ",format(last(dataset[,1]),dateform),",Source:",dataSource,sep=""))
     g<-g+theme(axis.text.x=element_text(size=15,face="plain",angle=90,hjust=1,vjust=0.5))
     g<-g+theme(axis.text.y=element_text(size=15,face="plain"))
     g<-g+theme(title      =element_text(size=15,face="plain"))
